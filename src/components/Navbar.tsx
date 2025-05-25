78490029f9a37c0f80f030971806c74290b075be
Sef2002 <yousefnakhleh02@gmail.com>
2025-05-25 17:13:35 +0200

    Update business name and navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,12 @@ const Navbar: React.FC = () => {
       <div className="container mx-auto px-4 md:px-8">
         <nav className="flex justify-between items-center bg-white rounded-[40px] px-8 py-4">
           <NavLink to="/" className="text-2xl font-heading text-black">
-            <img src="/assets/logo.png" alt="Prestige Salon Logo" className="h-12" />
+            <img 
+              src="/assets/logo.png" 
+              alt="Prestige Salon Logo" 
+              className="h-12 transform scale-125" 
+              style={{ transformOrigin: 'left center' }}
+            />
           </NavLink>
 
           {/* Desktop Menu */}
